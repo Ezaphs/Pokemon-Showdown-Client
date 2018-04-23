5a47f9e5a98e97efe2783d61effedd979b72bcf3
Ezaphs <38670231+Ezaphs@users.noreply.github.com>
2018-04-23 21:21:20 +0100

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -2,10 +2,10 @@ Config.origindomain = 'play.pokemonshowdown.com';
 // `defaultserver` specifies the server to use when the domain name in the
 // address bar is `Config.origindomain`.
 Config.defaultserver = {
-	id: 'dragonheaven',
-	host: '51.15.6.79',
-	port: 8050,
-	httpport: 8050,
+	id: 'showdown',
+	host: 'sim2.psim.us',
+	port: 443,
+	httpport: 8000,
 	altport: 80,
 	registered: true
 };
